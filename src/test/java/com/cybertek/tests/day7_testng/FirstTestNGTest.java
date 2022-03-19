6c01e590162d92988fd5c02f8d8686aeb8edc625
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-03-19 19:46:12 +0100

    TestNg Added

--- src/test/java/com/cybertek/tests/day7_testng/FirstTestNGTest.java
+++ src/test/java/com/cybertek/tests/day7_testng/FirstTestNGTest.java
@@ -1,6 +1,6 @@
 package com.cybertek.tests.day7_testng;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class FirstTestNGTest {
     @Test
@@ -12,4 +12,24 @@ public class FirstTestNGTest {
         System.out.println("My second Test Case");
     }
     @Test
+    public void test3(){
+        System.out.println("My third Test Case");
+    }
+    @BeforeMethod
+    public void setup(){
+        System.out.println("Before Method");
+    }
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("After Method");
+    }
+    @BeforeClass
+    public void classSetup(){
+        System.out.println("Before Class");
+    }
+    @AfterClass
+    public void classTearDown(){
+        System.out.println("After Class");
+    }
+
 }
