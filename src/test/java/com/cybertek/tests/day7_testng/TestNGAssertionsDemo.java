7c9e4617193692543430e779d121ad13c4b4f220
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-03-22 18:41:06 +0100

    Assert class

--- src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
+++ src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
@@ -1,10 +1,14 @@
 package com.cybertek.tests.day7_testng;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNGAssertionsDemo {
 
+    @BeforeMethod
+    public void beforeMethod(){ System.out.println("Open browser");}
     @Test
     public void test1(){
         System.out.println("First Assertion");
@@ -12,4 +16,39 @@ public class TestNGAssertionsDemo {
         System.out.println("Second Assertion");
         Assert.assertEquals("url","url");
     }
+    @Test
+    public void test2(){
+        Assert.assertEquals("test2","test2");
+    }
+    @Test
+    public void test3(){
+        String expectedTitle = "Cyb";
+        String actualTitle = "Cybertek";
+
+        // If we use 'if' statement
+//        if (actualTitle.startsWith(expectedTitle))
+        // Condition will be true or false
+
+        Assert.assertTrue(actualTitle.startsWith(expectedTitle), "Verify title starts with Cyb");
+    }
+    @Test
+    public void test4(){
+        // Verify email contains @ sign
+        String email = "john@gmail.com";
+//        if (email.contains("@"))
+        Assert.assertTrue(email.contains("@"), "Verify email contains @ sign");
+    }
+    @Test
+    public void test5(){
+        String email = "johngmail.com";
+        Assert.assertFalse(email.contains("@"));
+        // Condition is false but returns true
+    }
+    @Test
+    public void test6(){
+        Assert.assertNotEquals("Td", "td");
+    }
+
+    @AfterMethod
+    public void afterMethod(){ System.out.println("Close browser"); }
 }
