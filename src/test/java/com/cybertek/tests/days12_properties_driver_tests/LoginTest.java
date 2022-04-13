0c7597dc7da48440641b9d89db9934df2f993874
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-04-13 18:01:09 +0200

    Login Page Test

--- src/test/java/com/cybertek/tests/days12_properties_driver_tests/LoginTest.java
+++ src/test/java/com/cybertek/tests/days12_properties_driver_tests/LoginTest.java
@@ -2,16 +2,10 @@ package com.cybertek.tests.days12_properties_driver_tests;
 
 import com.cybertek.tests.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
-import com.cybertek.utilities.Driver;
-import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
 public class LoginTest extends TestBase {
 
