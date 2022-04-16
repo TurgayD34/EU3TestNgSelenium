4fd1453f15fff6330e264e5161f75acdd5155c5b
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-04-16 14:31:15 +0200

    Verify Test cases use DashboardPage

---
+++ src/test/java/com/cybertek/pages/DashboardPage.java
@@ -0,0 +1,14 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DashboardPage extends BasePage{
+
+
+
+}
