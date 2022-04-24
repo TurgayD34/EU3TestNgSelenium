925e4ef53b17e5ff18d69fb992c461e9642e94af
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-04-24 15:49:28 +0200

    Assert the Page Subtitles

--- src/test/java/com/cybertek/tests/days13_pom/PageSubtitleTest.java
+++ src/test/java/com/cybertek/tests/days13_pom/PageSubtitleTest.java
@@ -5,6 +5,7 @@ import com.cybertek.pages.CalenderEventsPage;
 import com.cybertek.pages.DashboardPage;
 import com.cybertek.pages.LoginPage;
 import com.cybertek.tests.TestBase;
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Verify;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -23,27 +24,33 @@ public class PageSubtitleTest extends TestBase {
     @Test
     public void VerifySubtitles(){
         LoginPage loginPage = new LoginPage();
+
         loginPage.loginAsDriver();
 
         DashboardPage dashboardPage = new DashboardPage();
 
         //Verify that page subtitle is Quick Launchpad
+
         String expected = "Quick Launchpad";
+
         String actual = dashboardPage.getPageSubTitle();
+
         Assert.assertEquals(actual, expected, "verify subtitle");
+
         expected = "Quick Launchpadd";
+
         Assert.assertNotEquals(actual, expected, "Verify not equals");
 
         //Verify that page subtitle is Calendar Events
-        dashboardPage.navigateToModule("Activities", "Calendar Events");
-//        expected = "Calendar Events";
-//        actual = dashboardPage.getPageSubTitle();
-//        Assert.assertEquals(actual, expected, "Verify Calendar Events");
-//        expected = "Calender Event";
-//        Assert.assertNotEquals(actual, expected, "Verify not equals");
-        CalenderEventsPage calenderEventsPage = new CalenderEventsPage();
-        Assert.assertEquals(calenderEventsPage.getPageSubTitle(),"Calendar Events", "Verify Calendar Events");
-        //Both ways are working well
 
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        CalenderEventsPage calenderEventsPage = new CalenderEventsPage();
+
+        BrowserUtils.waitFor(2);
+
+        //If the calenderEvents page working no need to use waitFor
+
+        Assert.assertEquals(calenderEventsPage.getPageSubTitle(),"Calendar Events", "Verify Calendar Events");
     }
 }
