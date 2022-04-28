defa67b18c16300f19c0feb5ed07df7c388a451a
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-04-28 09:27:03 +0200

    Added BeforeTest and AfterTest for report

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import com.cybertek.utilities.WebDriverFactory;
@@ -7,7 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +21,33 @@ public class TestBase {
     protected WebDriver driver;
     protected Actions actions;
     protected WebDriverWait wait;
+    //This 3 parts copy paste from TestBase
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
+
+    @BeforeTest //This part also copy paste from TestBase
+    public void setUpTest(){
+        report = new ExtentReports();
+
+        String projectPath = System.getProperty("user.dir");
+        String path = projectPath + "/test-output/report.html";
+
+        htmlReporter = new ExtentHtmlReporter(path);
+
+        report.attachReporter(htmlReporter);
+
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
+
+        report.setSystemInfo("Environment","QA");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("Os",System.getProperty("os.name"));
+    }
+
+    @AfterTest
+    public void tearDownTest(){
+        report.flush();
+    }
 
     @BeforeMethod
     public void setUp() {
