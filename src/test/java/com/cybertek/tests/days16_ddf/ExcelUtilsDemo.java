2209973c8d913086958561721c0604f3f45e2c42
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-04 20:12:20 +0200

    ExtentDemoTest

---
+++ src/test/java/com/cybertek/tests/days16_ddf/ExcelUtilsDemo.java
@@ -0,0 +1,36 @@
+package com.cybertek.tests.days16_ddf;
+
+import com.cybertek.utilities.ExcelUtil;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelUtilsDemo {
+
+    @Test
+    public void readExcelFile(){
+
+        //Create an object from ExcelUtil
+        //It accepts two arguments
+        //Argument 1: Location of the file (path)
+        //Argument 2: Sheet that we want to open (sheetName)
+
+        ExcelUtil qa3Sheet = new ExcelUtil("src/test/resources/uye.xlsx", "QA3-short");
+
+        //How many rows in the sheet
+        System.out.println("qa3Sheet.rowCount() = " + qa3Sheet.rowCount());
+        //How many columns in the sheet
+        System.out.println("qa3Sheet.columnCount() = " + qa3Sheet.columnCount());
+        //Get all column names
+        System.out.println("qa3Sheet.getColumnsNames() = " + qa3Sheet.getColumnsNames());
+
+        //Get all data in list of maps
+        List<Map<String, String>> dataList = qa3Sheet.getDataList();
+        for (Map<String, String> onerow : dataList) {
+            System.out.println("onerow = " + onerow);
+        }
+        //get a name as a value
+        System.out.println("dataList.get(2) = " + dataList.get(2).get("firstname"));
+    }
+}
