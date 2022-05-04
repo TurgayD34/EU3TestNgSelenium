081e16588ff75fc6deb790e131a33fe74b79a78e
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-04 20:11:46 +0200

    ContactInfoPage

---
+++ src/test/java/com/cybertek/pages/Contacts/ContactInfoPage.java
@@ -0,0 +1,17 @@
+package com.cybertek.pages.Contacts;
+
+import com.cybertek.pages.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ContactInfoPage extends BasePage {
+
+    @FindBy(css = "div.pull-left>h1.user-name")
+    public WebElement fullName;
+
+    @FindBy(xpath = "//a[@class='phone']")
+    public WebElement phone;
+
+    @FindBy(xpath = "//a[@class='email']")
+    public WebElement email;
+}
