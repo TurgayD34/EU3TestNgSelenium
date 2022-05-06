9e3436c782ed504af20eb1b977878c1371bfa7af
Turgay Demir <89524827+TurgayD34@users.noreply.github.com>
2022-05-06 11:52:43 +0200

    Dashboard Page

--- src/test/java/com/cybertek/pages/DashboardPage.java
+++ src/test/java/com/cybertek/pages/DashboardPage.java
@@ -1,11 +1,10 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 
 public class DashboardPage extends BasePage{
 
